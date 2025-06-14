22d092f239c0e18db1281468a00523aabfd4ece8
fernando4974 <fernando4974@gmail.com>
2025-06-13 19:12:39 -0500

    primer commit/ database.js y app.js parcialmente

---
+++ src/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express=require('express');
+
+const {TestConnection}=require('./config/database');
+
+const app=express();
+
+app.use(express.json());
+
+PORT=process.env.APP_PORT || 3000;
+
+app.get('/',(req,res)=>{
+
+    res.send("API de productos funcionando");
+})
+ async function StartServer() {
+
+    await TestConnection();
+
+    console.log(`API en el puerto${PORT}`);
+    console.log(`accede a la appi en  http://localhost:${PORT}`);
+    
+
+ }
+ StartServer();
---
+++ src/config/database.js
@@ -0,0 +1,36 @@
+console.log("hola mundo");
+
+require ('dotenv').config();
+
+const {Sequelize}= require('sequelize');
+
+    const sequelize = new Sequelize(
+        process.env.DB_DATBASE,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+
+        {
+            host:process.env.DB_HOST,
+            port:process.env.DB_PORT,
+            dialect:"postgres",
+            logging:false
+        }
+    );
+
+    async function TestConnection(){
+
+        try {
+
+        await sequelize.authenticate();
+        console.log("Data base connection succesfull");
+        
+        } catch (error) {
+
+            console.error("Data base connectio unsuccesfull");
+            process.exit(1);
+            
+        }
+
+    }
+    module.exports={ sequelize,TestConnection };
+
