9f6b1ab4a1fcf77318d2ae017ce06a3c385f1b45
fernando4974 <fernando4974@gmail.com>
2025-06-14 17:28:03 -0500

    falto el app.listen

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express=require('express');
 
 const {TestConnection}=require('./config/database');
 
+
 const app=express();
 
 app.use(express.json());
@@ -11,15 +12,17 @@ PORT=process.env.APP_PORT || 3000;
 
 app.get('/',(req,res)=>{
 
-    res.send("API de productos funcionando");
+    res.send("API de productos funcionando api-1");
 })
  async function StartServer() {
 
     await TestConnection();
-
-    console.log(`API en el puerto${PORT}`);
-    console.log(`accede a la appi en  http://localhost:${PORT}`);
+    app.listen(PORT,()=>{
+    
+    console.log(`API en el puerto ${PORT}`);
+    console.log(`accede a la appi en  http://localhost: ${PORT}`);
     
 
- }
+ })
+}
  StartServer();
